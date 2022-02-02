ff58c4417abfb74354129814eb97f7af7988f0e2
DrTimZen <tim.zen@icloud.com>
2022-02-02 09:18:51 +0100

    alternative dice change

--- script.js
+++ script.js
@@ -3,31 +3,38 @@
 const button = document.querySelector("button");
 const player1 = document.querySelector(".player1");
 const player2 = document.querySelector(".player2");
+const dice1 = document.querySelector("#dice1");
+const dice2 = document.querySelector("#dice2");
 
 // const container = document.querySelector("");
 
 const diceRoll = () => Math.floor(Math.random() * 6) + 1;
 
 button.addEventListener("click", function (e) {
-  insertNewDice();
+  changeDice();
 });
 
-const insertNewDice = function () {
-  document.getElementById("BornToRoll").play();
-  document
-    .querySelectorAll(".dice-image")
-    .forEach((e) => e.classList.add("hidden"));
-  document.querySelectorAll(".dice-image").forEach((e) => e.remove());
-  player1.insertAdjacentHTML(
-    "afterbegin",
-    `
-    <img class="dice-image" src="images/dice-${diceRoll()}.png" /> 
-    `
-  );
-  player2.insertAdjacentHTML(
-    "afterbegin",
-    `
-        <img class="dice-image" src="images/dice-${diceRoll()}.png" /> 
-        `
-  );
+// const insertNewDice = function () {
+//   document.getElementById("BornToRoll").play();
+//   document
+//     .querySelectorAll(".dice-image")
+//     .forEach((e) => e.classList.add("hidden"));
+//   document.querySelectorAll(".dice-image").forEach((e) => e.remove());
+//   player1.insertAdjacentHTML(
+//     "afterbegin",
+//     `
+//     <img class="dice-image" src="images/dice-${diceRoll()}.png" />
+//     `
+//   );
+//   player2.insertAdjacentHTML(
+//     "afterbegin",
+//     `
+//         <img class="dice-image" src="images/dice-${diceRoll()}.png" />
+//         `
+//   );
+// };
+
+const changeDice = () => {
+  dice1.setAttribute("src", `images/dice-${diceRoll()}.png`);
+  dice2.setAttribute("src", `images/dice-${diceRoll()}.png`);
 };
